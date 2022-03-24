e8d3e626650d839e7406ec783138c05dc519154f
gasouza <gabril.strakh@gmail.com>
2022-03-24 20:15:36 +0100

    ex08: resolução do exercício

---
+++ ex08/ft_is_negative.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_is_negative.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gasouza <gasouza@student.42sp.org.br>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/24 20:12:25 by gasouza           #+#    #+#             */
+/*   Updated: 2022/03/24 20:14:47 by gasouza          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_putchar(char letter);
+
+void	ft_is_negative(int n)
+{
+	if (n < 0)
+		ft_putchar('N');
+	else
+		ft_putchar('P');
+}
+
+// #include <unistd.h>
+// void	ft_putchar(char letter)
+// {
+// 	write(0, &letter, 1);
+// }
+
+// int main(void)
+// {
+// 	ft_is_negative(-10);
+// 	ft_is_negative(0);
+// 	ft_is_negative(14);
+// 	return (0);
+// }
