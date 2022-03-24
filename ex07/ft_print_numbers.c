a0f74340de3e9ffdde29590a8a1e5105e9397f69
gasouza <gabril.strakh@gmail.com>
2022-03-24 20:10:27 +0100

    ex07: resolução do exercício

---
+++ ex07/ft_print_numbers.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_numbers.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gasouza <gasouza@student.42sp.org.br>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/24 20:06:47 by gasouza           #+#    #+#             */
+/*   Updated: 2022/03/24 20:09:35 by gasouza          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_putchar(char letter);
+
+void	ft_print_numbers(void)
+{
+	char	num;
+
+	num = '0';
+	while (num <= '9')
+	{
+		ft_putchar(num);
+		num++;
+	}
+}
+
+// #include <unistd.h>
+// void	ft_putchar(char letter)
+// {
+// 	write(0, &letter, 1);
+// }
+
+// int main(void)
+// {
+// 	ft_print_numbers();
+// 	return (0);
+// }
