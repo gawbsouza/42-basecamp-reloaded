53b7e6524d07eac0c866ebe890673a4809b1f1f3
gasouza <gabril.strakh@gmail.com>
2022-03-29 22:04:54 +0200

    ex13: formatar conforme norminette

--- ex13/ft_recursive_factorial.c
+++ ex13/ft_recursive_factorial.c
@@ -6,17 +6,17 @@
 /*   By: gasouza <gasouza@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/24 21:27:49 by gasouza           #+#    #+#             */
-/*   Updated: 2022/03/24 21:32:24 by gasouza          ###   ########.fr       */
+/*   Updated: 2022/03/29 22:04:03 by gasouza          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int ft_recursive_factorial(int nb)
+int	ft_recursive_factorial(int nb)
 {
 	if (nb > 16 || nb < 0)
 		return (0);
 	if (nb == 0)
 		return (1);
-	return nb * ft_recursive_factorial(nb - 1);
+	return (nb * ft_recursive_factorial(nb - 1));
 }
 
 // #include <stdio.h>
