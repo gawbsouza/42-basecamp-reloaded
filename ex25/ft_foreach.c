60660d261703e84a675baed2268bdce167ea3d76
gasouza <gabril.strakh@gmail.com>
2022-04-05 19:45:15 +0200

    ex25: remover main()

--- ex25/ft_foreach.c
+++ ex25/ft_foreach.c
@@ -6,7 +6,7 @@
 /*   By: gasouza <gasouza@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/30 15:12:21 by gasouza           #+#    #+#             */
-/*   Updated: 2022/03/30 15:49:37 by gasouza          ###   ########.fr       */
+/*   Updated: 2022/04/05 19:34:19 by gasouza          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,16 +21,3 @@ void	ft_foreach(int *tab, int length, void (*f)(int))
 		index++;
 	}
 }
-
-// #include <stdio.h>
-// void	print_nb(int nb)
-// {
-// 	printf("%d\n", nb);
-// }
-// int	main(void)
-// {
-// 	int	numbers[] = {1, 234, 0, 4532, 88, 41, 24};
-// 	int	size = sizeof(numbers) / sizeof(int);
-// 	ft_foreach(numbers, size, &print_nb);
-// 	return (0);
-// }
