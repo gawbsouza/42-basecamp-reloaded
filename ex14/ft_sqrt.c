1c1af07322a86a3e1926caea2081bcb5e1657aa1
gasouza <gabril.strakh@gmail.com>
2022-04-05 19:41:13 +0200

    ex14: remover main()

--- ex14/ft_sqrt.c
+++ ex14/ft_sqrt.c
@@ -6,7 +6,7 @@
 /*   By: gasouza <gasouza@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/25 14:57:38 by gasouza           #+#    #+#             */
-/*   Updated: 2022/03/25 15:12:56 by gasouza          ###   ########.fr       */
+/*   Updated: 2022/04/05 19:32:21 by gasouza          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,13 +28,3 @@ int	ft_sqrt(int nb)
 		return (count);
 	return (0);
 }
-
-// #include <stdio.h>
-// int main(void)
-// {
-// 	int numbers[] = {0, -2, -49, 1, 9, 2, 4, 49, 25, 125};
-// 	int size = sizeof(numbers) / sizeof(int);
-// 	for(int i = 0; i < size; i++)	
-// 		printf("Raiz de %d = %d\n", numbers[i], ft_sqrt(numbers[i]));
-// 	return (0);
-// }
