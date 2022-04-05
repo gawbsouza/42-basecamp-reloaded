023e06fd94b90b877e6d584fb6c4f33006dfe418
gasouza <gabril.strakh@gmail.com>
2022-04-05 19:38:56 +0200

    ex11: remover main()

--- ex11/ft_div_mod.c
+++ ex11/ft_div_mod.c
@@ -6,7 +6,7 @@
 /*   By: gasouza <gasouza@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/24 20:47:09 by gasouza           #+#    #+#             */
-/*   Updated: 2022/03/24 20:51:01 by gasouza          ###   ########.fr       */
+/*   Updated: 2022/04/05 19:31:51 by gasouza          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,3 @@ void	ft_div_mod(int a, int b, int *div, int *mod)
 	*div = a / b;
 	*mod = a % b;
 }
-
-// #include <stdio.h>
-// int main(void)
-// {
-// 	int a = 100;
-// 	int b = 9;
-// 	int div;
-// 	int mod;
-// 	ft_div_mod(a, b, &div, &mod);
-// 	printf("a: %d, b: %d, div: %d, mod: %d\n", a, b, div, mod);
-// 	return (0);
-// }
